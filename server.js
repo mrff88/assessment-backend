e96fbec1c65b117c64191aabdaa14759329acd39
mrff88 <mrff88@gmail.com>
2022-04-13 11:56:10 -0500

    build: add dotenv config for test and dev env

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log('Hellor world');
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// config dotenv environments
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: path.resolve(__dirname, `${process.env.NODE_ENV}.env`),
+});
+
+console.log('hello world');
